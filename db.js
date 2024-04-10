60e95b715f2fdd707992f4689ccdfcbe4e61f97a
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-04-10 15:00:46 +0200

    Backend completed

--- db.js
+++ db.js
@@ -7,27 +7,6 @@ dotenv.config();
 // Load environment variables
 const { CONNECTION_URL } = process.env;
 
-// Connect to MongoDB
-// export const connectDB = async () => {
-//     try {
-//         await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-//         console.log('Connected to MongoDB');
-//     } catch (error) {
-//         console.error('Error connecting to MongoDB:', error);
-//     }
-// };
-
-// // Define schema for recipes
-// const recipeSchema = new mongoose.Schema({
-//     title: String,
-//     ingredients: [String],
-//     instructions: String,
-//     cookingTime: String
-// }, { collection: 'recipes' }); // Specify the collection name
-
-// // Create a model based on the schema
-// export const Recipe = mongoose.model('Recipe', recipeSchema);
-
 // Insert a new recipe
 export const insertRecipe = async (recipe) => {
     try {
